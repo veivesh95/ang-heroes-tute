43e38c013ae2d07119abb7322a4d4e1a106df562
Veivesh <veivesh95@gmail.com>
2017-12-03 20:05:43 +0530

    Latest changes

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,13 @@
+import { Hero } from "./hero";
+export const HEROES: Hero[] = [
+    { id: 11, name: 'Batman' },
+    { id: 12, name: 'Superman' },
+    { id: 13, name: 'Green Lantern' },
+    { id: 14, name: 'Wonder Woman' },
+    { id: 15, name: 'The Flash' },
+    { id: 16, name: 'Aquaman' },
+    { id: 17, name: 'Cyborg' },
+    { id: 18, name: 'Martian Manhunter' },
+    { id: 19, name: 'Green Arrow' },
+    { id: 20, name: 'Tornado' }
+];
